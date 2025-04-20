e7a7755d0def273e01162bf33762000722ec801e
trungcu-tech <cu.le@rikai.technology>
2025-04-20 14:06:30 +0700

    Initial commit

---
+++ awwwards-next/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>scorll animations</h2>
+      <p><Link href="/techniques-animation/scroll/background-image-parallax/001">001</Link></p>
+    </div>
+  );
+}
